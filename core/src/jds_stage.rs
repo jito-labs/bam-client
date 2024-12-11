3d2aa8fe1960a87ad1a77873f6f55442f0d9478c
David Gorski <david@jito.wtf>
2024-12-11 15:45:26 -0500

    skellington

--- core/src/jds_stage.rs
+++ core/src/jds_stage.rs
@@ -1,13 +1,17 @@
-use std::sync::{atomic::AtomicBool, Arc, RwLock};
+use std::{sync::{atomic::AtomicBool, Arc, RwLock}, thread::Builder};
 
 use crossbeam_channel::Receiver;
+use jito_protos::proto::jds_api::validator_api_client::ValidatorApiClient;
 use solana_perf::packet::PacketBatch;
 use solana_poh::poh_recorder::PohRecorder;
 use solana_runtime::bank_forks::BankForks;
+use tokio::time::timeout;
 
 use crate::sigverify::SigverifyTracerPacketStats;
 
-pub(crate) struct JdsStage {}
+pub(crate) struct JdsStage {
+    threads: Vec<std::thread::JoinHandle<()>>,
+}
 
 impl JdsStage {
     pub fn new(
@@ -18,6 +22,37 @@ impl JdsStage {
         bank_forks: Arc<RwLock<BankForks>>,
         exit: Arc<AtomicBool>,
     ) -> Self {
-        JdsStage {}
+        let api_connection_thread = Builder::new()
+            .name("block-engine-stage".to_string())
+            .spawn(move || {
+                let rt = tokio::runtime::Builder::new_multi_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap();
+                rt.block_on(Self::start_service(
+                    jds_url,
+                    jds_enabled,
+                    exit,
+                ));
+            })
+            .unwrap();
+
+
+        JdsStage {
+            threads: vec![
+                api_connection_thread
+            ],
+        }
+    }
+
+    async fn start_service(
+        jds_url: String,
+        jds_enabled: Arc<AtomicBool>,
+        exit: Arc<AtomicBool>,
+    ) {
+        let backend_endpoint = tonic::transport::Endpoint::from_shared(jds_url).unwrap();
+        let connection_timeout = std::time::Duration::from_secs(5);
+        let block_engine_channel = timeout(connection_timeout, backend_endpoint.connect()).await.unwrap().unwrap();
+        let validator_client = ValidatorApiClient::new(block_engine_channel);
     }
 }
