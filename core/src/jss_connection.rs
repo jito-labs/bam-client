eb10d0cd1b86a75422271d3f7805e1bded3d9fe9
David Gorski <david@jito.wtf>
2025-02-05 22:15:16 -0500

    connection clean-up

--- core/src/jss_connection.rs
+++ core/src/jss_connection.rs
@@ -17,13 +17,10 @@ use tokio::time::{interval, timeout};
 // Keeps track of last received heartbeat 'behind the scenes' and will mark itself as unhealthy if no heartbeat is received
 pub struct JssConnection {
     outbound_sender: mpsc::UnboundedSender<StartSchedulerMessage>,
-
-    its_over: bool,
     last_heartbeat: Arc<Mutex<std::time::Instant>>,
     tpu_config: Arc<Mutex<Option<TpuConfigResp>>>,
     microblock_receiver: Receiver<MicroBlock>,
-
-    heartbeat_task: tokio::task::JoinHandle<()>,
+    background_task: tokio::task::JoinHandle<()>,
 }
 
 impl JssConnection {
@@ -42,68 +39,76 @@ impl JssConnection {
         let (outbound_sender, outbound_receiver) = mpsc::unbounded();
         let outbound_stream =
             tonic::Request::new(outbound_receiver.map(|req: StartSchedulerMessage| req));
-        let mut inbound_stream = validator_client
+        let inbound_stream = validator_client
             .start_scheduler_stream(outbound_stream)
             .await
             .ok()?
             .into_inner();
 
         let last_heartbeat = Arc::new(Mutex::new(std::time::Instant::now()));
-        let last_heartbeat_clone = last_heartbeat.clone();
-        let sender_clone = outbound_sender.clone();
-        let tpu_config_clone = tpu_config.clone();
         let (microblock_sender, microblock_receiver) = crossbeam_channel::unbounded();
-        let heartbeat_task = tokio::spawn(async move {
-            loop {
-                let mut heartbeat_interval = interval(std::time::Duration::from_secs(5));
-                tokio::select! {
-                    _ = heartbeat_interval.tick() => {
-                        let _ = sender_clone.unbounded_send(StartSchedulerMessage {
-                            msg: Some(Msg::HeartBeat(HeartBeat {
-                                pubkey: pubkey.to_string(),
-                            })),
-                        });
-                        validator_client.get_tpu_config(GetTpuConfigRequest {}).await.unwrap().map(|resp| {
-                            *tpu_config_clone.lock().unwrap() = Some(resp);
-                        });
-                    }
-                    inbound = inbound_stream.next() => {
-                        let Some(inbound) = inbound else {
-                            break;
-                        };
-                        let Ok(inbound) = inbound else {
-                            break;
-                        };
-
-                        match inbound {
-                            StartSchedulerResponse { resp: Some(Resp::HeartBeat(_)), .. } => {
-                                *last_heartbeat_clone.lock().unwrap() = std::time::Instant::now();
-                            }
-                            StartSchedulerResponse { resp: Some(Resp::MicroBlock(microblock)), .. } => {
-                                let _ = microblock_sender.send(microblock);
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-
-                let _ = sender_clone.unbounded_send(StartSchedulerMessage {
-                    msg: Some(Msg::HeartBeat(HeartBeat {
-                        pubkey: pubkey.to_string(),
-                    })),
-                });
-                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-            }
-        });
+        let background_task = tokio::spawn(Self::background_task(
+            inbound_stream,
+            outbound_sender.clone(),
+            validator_client,
+            pubkey,
+            last_heartbeat.clone(),
+            tpu_config.clone(),
+            microblock_sender,
+        ));
 
         Some(Self {
             outbound_sender,
-            its_over: false,
             last_heartbeat,
             tpu_config,
-            heartbeat_task,
+            background_task,
             microblock_receiver,
         })
+    }
+
+    async fn background_task(
+        mut inbound_stream: tonic::Streaming<StartSchedulerResponse>,
+        sender_clone: mpsc::UnboundedSender<StartSchedulerMessage>,
+        mut validator_client: JssNodeApiClient<tonic::transport::channel::Channel>,
+        pubkey: Pubkey,
+        last_heartbeat_clone: Arc<Mutex<std::time::Instant>>,
+        tpu_config_clone: Arc<Mutex<Option<TpuConfigResp>>>,
+        microblock_sender: crossbeam_channel::Sender<MicroBlock>,
+    ) {
+        let mut heartbeat_interval = interval(std::time::Duration::from_secs(5));
+        loop {
+            tokio::select! {
+                _ = heartbeat_interval.tick() => {
+                    let _ = sender_clone.unbounded_send(StartSchedulerMessage {
+                        msg: Some(Msg::HeartBeat(HeartBeat {
+                            pubkey: pubkey.to_string(),
+                        })),
+                    });
+                    let Ok(resp) = validator_client.get_tpu_config(GetTpuConfigRequest {}).await else {
+                        break;
+                    };
+                    *tpu_config_clone.lock().unwrap() = Some(resp.into_inner());
+                }
+                inbound = inbound_stream.message() => {
+                    let Ok(inbound) = inbound else {
+                        break;
+                    };
+                    let Some(inbound) = inbound else {
+                        break;
+                    };
+
+                    match inbound {
+                        StartSchedulerResponse { resp: Some(Resp::HeartBeat(_)), .. } => {
+                            *last_heartbeat_clone.lock().unwrap() = std::time::Instant::now();
+                        }
+                        StartSchedulerResponse { resp: Some(Resp::MicroBlock(microblock)), .. } => {
+                            let _ = microblock_sender.send(microblock);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
     }
 
     pub fn try_recv_microblock(&mut self) -> Option<MicroBlock> {
@@ -119,12 +124,22 @@ impl JssConnection {
 
     // Check if the connection is healthy
     pub fn is_healthy(&mut self) -> bool {
-        let is_healthy = !self.its_over
-            && self.last_heartbeat.lock().unwrap().elapsed() < std::time::Duration::from_secs(6);
+        let is_healthy = self.last_heartbeat.lock().unwrap().elapsed() < std::time::Duration::from_secs(6);
+
+        if !is_healthy {
+            info!("dead_heartbeat={}", self.last_heartbeat.lock().unwrap().elapsed().as_secs());
+        }
+
         is_healthy
     }
 
     pub fn get_tpu_config(&self) -> Option<TpuConfigResp> {
         self.tpu_config.lock().unwrap().clone()
+    }
+}
+
+impl Drop for JssConnection {
+    fn drop(&mut self) {
+        self.background_task.abort();
     }
 }
