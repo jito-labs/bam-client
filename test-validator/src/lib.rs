a50005a3195475a83b97d2f6a1522390d310b197
David Gorski <david@jito.wtf>
2025-04-25 17:05:55 -0400

    Fix admin usage (it works!)

--- test-validator/src/lib.rs
+++ test-validator/src/lib.rs
@@ -136,7 +136,7 @@ pub struct TestValidatorGenesis {
     pub tpu_enable_udp: bool,
     pub geyser_plugin_manager: Arc<RwLock<GeyserPluginManager>>,
     admin_rpc_service_post_init: Arc<RwLock<Option<AdminRpcRequestMetadataPostInit>>>,
-    pub jss_url: Option<String>,
+    pub jss_url: Arc<Mutex<Option<String>>>,
 }
 
 impl Default for TestValidatorGenesis {
@@ -170,7 +170,7 @@ impl Default for TestValidatorGenesis {
             geyser_plugin_manager: Arc::new(RwLock::new(GeyserPluginManager::new())),
             admin_rpc_service_post_init:
                 Arc::<RwLock<Option<AdminRpcRequestMetadataPostInit>>>::default(),
-            jss_url: None,
+            jss_url: Arc::new(Mutex::new(None)),
         }
     }
 }
@@ -1034,7 +1034,7 @@ impl TestValidator {
             staked_nodes_overrides: config.staked_nodes_overrides.clone(),
             accounts_db_config,
             runtime_config,
-            jss_url: Arc::new(Mutex::new(config.jss_url.clone())),
+            jss_url: config.jss_url.clone(),
             ..ValidatorConfig::default_for_test()
         };
         if let Some(ref tower_storage) = config.tower_storage {
