061084ccbcdabc3eb008a8e9d83aa900141c16d8
David Gorski <david@jito.wtf>
2025-05-15 15:01:10 -0400

    More work

--- core/src/jss_dependencies.rs
+++ core/src/jss_dependencies.rs
@@ -1,7 +1,9 @@
 use std::sync::{atomic::AtomicBool, Arc, Mutex};
 
-use jito_protos::proto::{jss_api::{BuilderConfigResp, StartSchedulerMessage}, jss_types::Bundle};
+use jito_protos::proto::{jss_api::StartSchedulerMessage, jss_types::Bundle};
 use solana_gossip::cluster_info::ClusterInfo;
+
+use crate::proxy::block_engine_stage::BlockBuilderFeeInfo;
 
 
 #[derive(Clone)]
@@ -15,5 +17,5 @@ pub struct JssDependencies {
     pub outbound_receiver: crossbeam_channel::Receiver<StartSchedulerMessage>,
 
     pub cluster_info: Arc<ClusterInfo>,
-    pub builder_config: Arc<Mutex<Option<BuilderConfigResp>>>,
+    pub block_builder_fee_info: Arc<Mutex<BlockBuilderFeeInfo>>
 }
