20a01e7f9c097dc506c0d96fbce0df3fcbb10df8
David Gorski <david@jito.wtf>
2025-05-15 14:06:03 -0400

    WIP

--- core/src/jss_dependencies.rs
+++ core/src/jss_dependencies.rs
@@ -1,4 +1,14 @@
+use std::sync::{Arc, Mutex};
 
+use jito_protos::proto::{jss_api::BuilderConfigResp, jss_types::Bundle};
+
+
+#[derive(Debug, Clone)]
 pub struct JssDependencies {
-    
+    pub bundle_sender: crossbeam_channel::Sender<Bundle>,
+    pub bundle_receiver: crossbeam_channel::Receiver<Bundle>,
+
+    // TODO: outbound sender/receiver
+
+    pub builder_config: Arc<Mutex<Option<BuilderConfigResp>>>,
 }
