3f976b84a59004eb6cb27de2f49b89dbd61c0fbb
David Gorski <david@jito.wtf>
2025-05-17 16:00:56 -0400

    More work

--- core/src/banking_stage/transaction_scheduler/jss_receive_and_buffer.rs
+++ core/src/banking_stage/transaction_scheduler/jss_receive_and_buffer.rs
@@ -9,14 +9,22 @@ use jito_protos::proto::{
     jss_api::{start_scheduler_message::Msg, StartSchedulerMessage},
     jss_types::{bundle_result, Bundle, Packet},
 };
-use solana_runtime::{bank::Bank, bank_forks::BankForks};
+use solana_runtime::bank_forks::BankForks;
 use solana_runtime_transaction::runtime_transaction::RuntimeTransaction;
-use solana_sdk::{packet::{PacketFlags, PACKET_DATA_SIZE}, transaction::SanitizedTransaction};
+use solana_sdk::{
+    packet::{PacketFlags, PACKET_DATA_SIZE},
+    transaction::SanitizedTransaction,
+};
 
-use crate::banking_stage::{decision_maker::BufferedPacketsDecision, immutable_deserialized_packet::ImmutableDeserializedPacket, packet_deserializer::PacketDeserializer};
+use crate::banking_stage::{
+    decision_maker::BufferedPacketsDecision,
+    immutable_deserialized_packet::ImmutableDeserializedPacket,
+    packet_deserializer::PacketDeserializer,
+};
 
 use super::{
-    receive_and_buffer::{ReceiveAndBuffer, SanitizedTransactionReceiveAndBuffer}, transaction_state_container::TransactionStateContainer,
+    receive_and_buffer::{ReceiveAndBuffer, SanitizedTransactionReceiveAndBuffer},
+    transaction_state_container::TransactionStateContainer,
 };
 
 use crate::banking_stage::transaction_scheduler::transaction_state_container::StateContainer;
@@ -25,9 +33,6 @@ pub struct JssReceiveAndBuffer {
     jss_enabled: Arc<AtomicBool>,
     bundle_receiver: crossbeam_channel::Receiver<Bundle>,
     response_sender: Sender<StartSchedulerMessage>,
-    bank_forks: Arc<RwLock<BankForks>>,
-
-    // Leveraging this for now; limits us to just single packets; put thats ok
     internal_receive_and_buffer: SanitizedTransactionReceiveAndBuffer,
 }
 
@@ -42,19 +47,17 @@ impl JssReceiveAndBuffer {
             PacketDeserializer::new(unbounded().1),
             bank_forks.clone(),
             false,
-            Arc::new(AtomicBool::new(false)), // This internal receive and buffer is not JSS enabled (We want it to process packets for us)
+            Arc::new(AtomicBool::new(false)),
         );
         Self {
             jss_enabled,
             bundle_receiver,
             response_sender,
-            bank_forks,
             internal_receive_and_buffer,
         }
     }
 
     fn parse_transactions<'a>(
-        _bank: &Bank,
         packets: impl Iterator<Item = &'a Packet>,
     ) -> Vec<ImmutableDeserializedPacket> {
         packets
@@ -62,7 +65,6 @@ impl JssReceiveAndBuffer {
                 if packet.data.len() > PACKET_DATA_SIZE {
                     return None;
                 }
-
                 let mut solana_packet = solana_sdk::packet::Packet::default();
                 solana_packet.meta_mut().size = packet.data.len() as usize;
                 solana_packet.meta_mut().set_discard(false);
@@ -92,26 +94,30 @@ impl JssReceiveAndBuffer {
                     }
                 }
                 Some(ImmutableDeserializedPacket::new(solana_packet).ok()?)
-                //let sanitized_transaction = packet.build_sanitized_transaction(
-                //    bank.vote_only_bank(),
-                //    bank,
-                //    bank.get_reserved_account_keys(),
-                //)?;
-                //Some(sanitized_transaction.0)
             })
             .collect_vec()
     }
 
-    fn send_invalid_bundle_result(
-        &self,
-        seq_id: u32,
-    ) {
+    fn send_invalid_bundle_result(&self, seq_id: u32) {
         let _ = self.response_sender.try_send(StartSchedulerMessage {
             msg: Some(Msg::BundleResult(
                 jito_protos::proto::jss_types::BundleResult {
                     seq_id: seq_id,
                     result: Some(bundle_result::Result::Invalid(
                         jito_protos::proto::jss_types::Invalid {},
+                    )),
+                },
+            )),
+        });
+    }
+
+    fn send_retryable_bundle_result(&self, seq_id: u32) {
+        let _ = self.response_sender.try_send(StartSchedulerMessage {
+            msg: Some(Msg::BundleResult(
+                jito_protos::proto::jss_types::BundleResult {
+                    seq_id: seq_id,
+                    result: Some(bundle_result::Result::Retryable(
+                        jito_protos::proto::jss_types::Retryable {},
                     )),
                 },
             )),
@@ -136,17 +142,13 @@ impl ReceiveAndBuffer for JssReceiveAndBuffer {
 
         let mut result = 0;
         match decision {
-            BufferedPacketsDecision::Consume(_) |
-            BufferedPacketsDecision::Hold => {
+            BufferedPacketsDecision::Consume(_) | BufferedPacketsDecision::Hold => {
                 while let Ok(bundle) = self.bundle_receiver.try_recv() {
                     if bundle.packets.len() == 0 {
                         continue;
                     }
 
-                    let packets = Self::parse_transactions(
-                        &self.bank_forks.read().unwrap().working_bank(),
-                        bundle.packets.iter(),
-                    );
+                    let packets = Self::parse_transactions(bundle.packets.iter());
                     if packets.len() != bundle.packets.len() {
                         self.send_invalid_bundle_result(bundle.seq_id);
                         continue;
@@ -168,51 +170,52 @@ impl ReceiveAndBuffer for JssReceiveAndBuffer {
                     };
 
                     let mut tmp_container = Self::Container::with_capacity(bundle.packets.len());
-                    self.internal_receive_and_buffer.buffer_packets(&mut tmp_container, timing_metrics, count_metrics, packets);
+                    self.internal_receive_and_buffer.buffer_packets(
+                        &mut tmp_container,
+                        timing_metrics,
+                        count_metrics,
+                        packets,
+                    );
 
                     let mut packets = vec![];
+                    let mut cost: u64 = 0;
                     let mut transaction_ttls = vec![];
                     for _ in 0..bundle.packets.len() {
                         let Some(id) = tmp_container.pop() else {
                             self.send_invalid_bundle_result(bundle.seq_id);
-                            continue;
+                            break;
                         };
                         let Some(entry) = tmp_container.get_mut_transaction_state(id.id) else {
                             self.send_invalid_bundle_result(bundle.seq_id);
-                            continue;
+                            break;
                         };
                         let Some(packet) = entry.packet().cloned() else {
                             self.send_invalid_bundle_result(bundle.seq_id);
-                            continue;
+                            break;
                         };
+                        cost = cost.saturating_add(entry.cost());
                         transaction_ttls.push(entry.transition_to_pending());
                         packets.push(packet);
+                    }
+                    if packets.len() != bundle.packets.len() {
+                        self.send_invalid_bundle_result(bundle.seq_id);
+                        continue;
                     }
 
                     container.insert_new_batch(
                         transaction_ttls,
                         packets,
                         u64::MAX.saturating_sub(bundle.seq_id as u64),
-                        1, // TODO_DG?
+                        cost,
                         revert_on_error,
                     );
 
                     result += 1;
                 }
             }
-            BufferedPacketsDecision::ForwardAndHold
-            | BufferedPacketsDecision::Forward => {
+            BufferedPacketsDecision::ForwardAndHold | BufferedPacketsDecision::Forward => {
                 while let Ok(bundle) = self.bundle_receiver.try_recv() {
-                    let _ = self.response_sender.try_send(StartSchedulerMessage {
-                        msg: Some(Msg::BundleResult(
-                            jito_protos::proto::jss_types::BundleResult {
-                                seq_id: bundle.seq_id as u32,
-                                result: Some(bundle_result::Result::Retryable(
-                                    jito_protos::proto::jss_types::Retryable {},
-                                )),
-                            },
-                        )),
-                    });
+                    self.send_retryable_bundle_result(bundle.seq_id);
                 }
             }
         }
