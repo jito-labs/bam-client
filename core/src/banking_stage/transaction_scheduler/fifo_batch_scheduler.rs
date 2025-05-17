23b6a3932f0c69e5e290a09bd852014f57028231
David Gorski <david@jito.wtf>
2025-05-17 14:50:53 -0400

    Something is working

--- core/src/banking_stage/transaction_scheduler/fifo_batch_scheduler.rs
+++ core/src/banking_stage/transaction_scheduler/fifo_batch_scheduler.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 use ahash::HashMap;
 use crossbeam_channel::{Receiver, Sender};
 use jito_protos::proto::jss_api::{start_scheduler_message::Msg, StartSchedulerMessage};
-use prio_graph::{AccessKind, GraphNode, PrioGraph, TopLevelId};
+use prio_graph::{AccessKind, GraphNode, PrioGraph};
 use solana_pubkey::Pubkey;
 use solana_runtime_transaction::transaction_with_meta::TransactionWithMeta;
 use solana_svm_transaction::svm_message::SVMMessage;
@@ -34,8 +34,7 @@ pub struct FifoBatchScheduler<Tx: TransactionWithMeta> {
     finished_consume_work_receiver: Receiver<FinishedConsumeWork<Tx>>,
     response_sender: Sender<StartSchedulerMessage>,
 
-    next_batch_id: u64,
-    priority_ids: HashMap<usize, TransactionPriorityId>,
+    priority_ids: HashMap<TransactionBatchId, TransactionPriorityId>,
     prio_graph: SchedulerPrioGraph,
 }
 
@@ -49,27 +48,29 @@ impl<Tx: TransactionWithMeta> FifoBatchScheduler<Tx> {
             consume_work_senders,
             finished_consume_work_receiver,
             response_sender,
-            next_batch_id: 0,
             priority_ids: HashMap::default(),
             prio_graph: PrioGraph::new(passthrough_priority),
         }
     }
 
     /// Gets accessed accounts (resources) for use in `PrioGraph`.
-    fn get_transaction_account_access(
-        transaction: &SanitizedTransactionTTL<impl SVMMessage>,
-    ) -> impl Iterator<Item = (Pubkey, AccessKind)> + '_ {
-        let message = &transaction.transaction;
-        message
-            .account_keys()
-            .iter()
-            .enumerate()
-            .map(|(index, key)| {
-                if message.is_writable(index) {
-                    (*key, AccessKind::Write)
-                } else {
-                    (*key, AccessKind::Read)
-                }
+    fn get_transactions_account_access<'a>(
+        transactions: impl Iterator<Item = &'a SanitizedTransactionTTL<impl SVMMessage + 'a>> + 'a,
+    ) -> impl Iterator<Item = (Pubkey, AccessKind)> + 'a {
+        transactions
+            .flat_map(|transaction| {
+                let message = &transaction.transaction;
+                message
+                    .account_keys()
+                    .iter()
+                    .enumerate()
+                    .map(|(index, key)| {
+                        if message.is_writable(index) {
+                            (*key, AccessKind::Write)
+                        } else {
+                            (*key, AccessKind::Read)
+                        }
+                    })
             })
     }
 }
@@ -85,47 +86,80 @@ impl<Tx: TransactionWithMeta> Scheduler<Tx> for FifoBatchScheduler<Tx> {
         let mut num_scheduled = 0;
 
         // Insert all incoming transactions into the prio-graph
-        while let Some(next_bundle_id) = container.pop() {
-            let Some(txn) = container.get_transaction_ttl(next_bundle_id.id) else {
+        while let Some(next_batch_id) = container.pop() {
+            let Some((batch_ids, _)) = container.get_batch(next_batch_id.id) else {
                 continue;
             };
-            self.prio_graph.insert_transaction(next_bundle_id, Self::get_transaction_account_access(&txn));
-            self.priority_ids.insert(
-                next_bundle_id.id,
-                next_bundle_id,
-            );
-            info!("inserted transaction to prio-graph");
+
+            let txns = batch_ids
+                .iter()
+                .filter_map(|txn_id| {
+                    container.get_transaction_ttl(*txn_id)
+                })
+                .collect::<Vec<_>>();
+            if txns.len() != batch_ids.len() {
+                container.push_ids_into_queue(std::iter::once(next_batch_id));
+                continue;
+            }
+
+            self.prio_graph.insert_transaction(next_batch_id, Self::get_transactions_account_access(txns.into_iter()));
+            info!("Pushed bundle into prio-graph");
         }
 
         // Schedule any available transactions in prio-graph
-        while let Some(next_id) = self.prio_graph.pop() {
-            // Get from container
-            let Some(txn) = container.get_mut_transaction_state(next_id.id) else {
+        while let Some(next_batch_id) = self.prio_graph.pop() {
+            let Some((batch_ids, revert_on_error)) = container.get_batch(next_batch_id.id) else {
+                info!("Batch {} not found in container", next_batch_id.id);
                 continue;
             };
-            let sanitized_transaction_ttl = txn.transition_to_pending();
-            
-            if !pre_lock_filter(&sanitized_transaction_ttl.transaction) {
-                continue;
-            }
+
+            info!("Scheduling batch {} revert_on_error={}", next_batch_id.id, revert_on_error);
+
+            let transactions = batch_ids
+                .iter()
+                .filter_map(|txn_id| {
+                    let result = container.get_mut_transaction_state(*txn_id)?;
+                    let result = result.transition_to_pending();
+                    if !pre_lock_filter(&result.transaction) {
+                        return None;
+                    }
+                    Some(result)
+                })
+                .collect::<Vec<_>>();
+    
+            let max_ages = transactions.iter().map(|txn| {
+                txn.max_age
+            }).collect::<Vec<_>>();
+
+            let transactions = transactions
+                .into_iter()
+                .map(|txn| {
+                    txn.transaction
+                })
+                .collect::<Vec<_>>();
 
             // Choose a completely random worker index (lol)
             let worker_index = rand::random::<usize>() % self.consume_work_senders.len();
             let consume_work_sender = &self.consume_work_senders[worker_index];
 
             let work = ConsumeWork {
-                batch_id: TransactionBatchId::new(self.next_batch_id),
-                ids: vec![next_id.id],
-                transactions: vec![sanitized_transaction_ttl.transaction],
-                max_ages: vec![sanitized_transaction_ttl.max_age],
-                revert_on_error: false,
-                respond_with_extra_info: false,
+                batch_id: TransactionBatchId::new(next_batch_id.id as u64),
+                ids: batch_ids,
+                transactions: transactions,
+                max_ages,
+                revert_on_error,
+                respond_with_extra_info: true,
             };
             // Send the work to the worker
             let _ = consume_work_sender
                 .send(work);
+            self.priority_ids.insert(
+                TransactionBatchId::new(next_batch_id.id as u64),
+                next_batch_id,
+            );
 
-            self.next_batch_id += 1;
+            info!("Sent batch {} to worker {}", next_batch_id.id, worker_index);
+
             num_scheduled += 1;
         }
 
@@ -143,17 +177,19 @@ impl<Tx: TransactionWithMeta> Scheduler<Tx> for FifoBatchScheduler<Tx> {
     ) -> Result<(usize, usize), SchedulerError> {
         let mut num_transactions = 0;
         while let Ok(result) = self.finished_consume_work_receiver.try_recv() {
+            info!("Received result for batch {}", result.work.batch_id);
             num_transactions += result.work.ids.len();
 
             // Send the result back to the scheduler
             if let Some(extra_info) = result.extra_info {
+                info!("Received extra info for batch {}", result.work.batch_id);
                 for (bundle_result, seq_id) in
                     extra_info.results.into_iter().zip(result.work.ids.iter())
                 {
                     let _ = self.response_sender.try_send(StartSchedulerMessage {
                         msg: Some(Msg::BundleResult(
                             jito_protos::proto::jss_types::BundleResult {
-                                seq_id: *seq_id as u32,
+                                seq_id: result.work.batch_id.0 as u32,
                                 result: Some(bundle_result),
                             },
                         )),
@@ -161,14 +197,23 @@ impl<Tx: TransactionWithMeta> Scheduler<Tx> for FifoBatchScheduler<Tx> {
                 }
             }
 
-            // Unblock the next blocked bundle in the prio-graph
-            for id in result.work.ids {
-                let Some(priority_id) = self.priority_ids.remove(&id) else {
-                    continue;
-                };
+            let batch_id = result.work.batch_id;
+            let Some(priority_id) = self.priority_ids.remove(&batch_id) else {
+                info!("Batch {} not found in priority ids", batch_id);
+                continue;
+            };
+            let Some((batch_ids, _)) = container.get_batch(priority_id.id) else {
+                info!("Batch {} not found in container", priority_id.id);
+                continue;
+            };
+            // Remove all the transactions
+            for id in batch_ids {
                 container.remove_by_id(id);
-                self.prio_graph.unblock(&priority_id);
             }
+
+            // Remove the batch
+            self.prio_graph.unblock(&priority_id);
+            container.remove_by_id(priority_id.id);
         }
 
         Ok((num_transactions, 0))
