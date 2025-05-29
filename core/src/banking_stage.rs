54a543cd7340e16ac3d8c482a70669968315f0a8
David Gorski <david@jito.wtf>
2025-05-29 10:58:35 -0400

    Move container cleaning to BufferAndReceive

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -725,7 +725,7 @@ impl BankingStage {
                             scheduler_worker_metrics,
                             forwarder,
                             scheduler_blacklisted_accounts,
-                            true,
+                            false,
                         );
 
                         match scheduler_controller.run() {
@@ -756,7 +756,7 @@ impl BankingStage {
                             scheduler_worker_metrics,
                             forwarder,
                             scheduler_blacklisted_accounts,
-                            true,
+                            false,
                         );
 
                         match scheduler_controller.run() {
@@ -838,7 +838,7 @@ impl BankingStage {
                             worker_metrics,
                             Option::<Forwarder<DummyLikeClusterInfo>>::None,
                             blacklisted_accounts.clone(),
-                            false,
+                            true,
                         );
 
                         match scheduler_controller.run() {
