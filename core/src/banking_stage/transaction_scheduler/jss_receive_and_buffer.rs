a9ae11811af35d5c7e1ade9e34b4748e134208a9
David Gorski <david@jito.wtf>
2025-06-10 11:25:40 -0400

    Change to 24

--- core/src/banking_stage/transaction_scheduler/jss_receive_and_buffer.rs
+++ core/src/banking_stage/transaction_scheduler/jss_receive_and_buffer.rs
@@ -187,7 +187,7 @@ impl ReceiveAndBuffer for JssReceiveAndBuffer {
         }
 
         let mut result = 0;
-        const MAX_BUNDLES_PER_RECV: usize = 10;
+        const MAX_BUNDLES_PER_RECV: usize = 24;
         match decision {
             BufferedPacketsDecision::Consume(_) | BufferedPacketsDecision::Hold => {
                 while result < MAX_BUNDLES_PER_RECV {
