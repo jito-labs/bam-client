16169616f48da23c08f0a9ea84967e549ba1f050
David Gorski <david@jito.wtf>
2025-06-09 20:39:53 -0400

    Fix compilation

--- core/src/banking_stage/consumer.rs
+++ core/src/banking_stage/consumer.rs
@@ -234,7 +234,8 @@ impl Consumer {
                 &payload.sanitized_transactions,
                 banking_stage_stats,
                 payload.slot_metrics_tracker,
-                reservation_cb
+                reservation_cb,
+                false,
             ));
         payload
             .slot_metrics_tracker
@@ -283,6 +284,7 @@ impl Consumer {
         banking_stage_stats: &BankingStageStats,
         slot_metrics_tracker: &mut LeaderSlotMetricsTracker,
         reservation_cb: &impl Fn(&Bank) -> u64,
+        revert_on_error: bool,
     ) -> ProcessTransactionsSummary {
         let (mut process_transactions_summary, process_transactions_us) = measure_us!(self
             .process_transactions(
@@ -343,6 +345,7 @@ impl Consumer {
         bank_creation_time: &Instant,
         transactions: &[impl TransactionWithMeta],
         reservation_cb: &impl Fn(&Bank) -> u64,
+        revert_on_error: bool,
     ) -> ProcessTransactionsSummary {
         let mut chunk_start = 0;
         let mut all_retryable_tx_indexes = vec![];
@@ -375,6 +378,7 @@ impl Consumer {
                 txs,
                 chunk_start,
                 reservation_cb,
+                revert_on_error,
             );
 
             let ProcessTransactionBatchOutput {
@@ -528,6 +532,7 @@ impl Consumer {
         txs: &[impl TransactionWithMeta],
         chunk_offset: usize,
         reservation_cb: &impl Fn(&Bank) -> u64,
+        revert_on_error: bool,
     ) -> ProcessTransactionBatchOutput {
         let mut error_counters = TransactionErrorMetrics::default();
         let pre_results = vec![Ok(()); txs.len()];
@@ -559,6 +564,7 @@ impl Consumer {
             chunk_offset,
             check_results.into_iter(),
             reservation_cb,
+            revert_on_error,
         );
 
         // Accumulate error counters from the initial checks into final results
@@ -630,6 +636,7 @@ impl Consumer {
         chunk_offset: usize,
         pre_results: impl Iterator<Item = Result<(), TransactionError>>,
         reservation_cb: &impl Fn(&Bank) -> u64,
+        revert_on_error: bool,
     ) -> ProcessTransactionBatchOutput {
         let (
             (transaction_qos_cost_results, cost_model_throttled_transactions_count),
@@ -662,7 +669,7 @@ impl Consumer {
         // WouldExceedMaxAccountCostLimit, WouldExceedMaxVoteCostLimit
         // and WouldExceedMaxAccountDataCostLimit
         let mut execute_and_commit_transactions_output =
-            self.execute_and_commit_transactions_locked(bank, &batch);
+            self.execute_and_commit_transactions_locked(bank, &batch, revert_on_error);
 
         // Once the accounts are new transactions can enter the pipeline to process them
         let (_, unlock_us) = measure_us!(drop(batch));
@@ -716,6 +723,7 @@ impl Consumer {
         &self,
         bank: &Arc<Bank>,
         batch: &TransactionBatch<impl TransactionWithMeta>,
+        revert_on_error: bool,
     ) -> ExecuteAndCommitTransactionsOutput {
         let transaction_status_sender_enabled = self.committer.transaction_status_sender_enabled();
         let mut execute_and_commit_timings = LeaderExecuteAndCommitTimings::default();
