689dbdfb668f4c6dbd09d9f60ae5d5e7c5bde4ac
Lucas Bruder <lucas@jito.wtf>
2025-07-10 11:32:02 -0500

    any ip

--- bam-local-cluster/src/cluster_manager.rs
+++ bam-local-cluster/src/cluster_manager.rs
@@ -69,7 +69,7 @@ impl BamLocalCluster {
 
             // Set hard-coded RPC addresses if specified
             if let (Some(rpc_port), Some(rpc_pubsub_port)) = (v.rpc_port, v.rpc_pubsub_port) {
-                let localhost_ip = IpAddr::V4(Ipv4Addr::LOCALHOST);
+                let localhost_ip = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
                 validator_config.rpc_addrs = Some((
                     SocketAddr::new(localhost_ip, rpc_port),
                     SocketAddr::new(localhost_ip, rpc_pubsub_port),
