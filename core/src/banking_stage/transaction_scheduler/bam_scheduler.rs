6d950a4426d578eb6d5c504a8fc4beb0ae41ee9b
David Gorski <david@jito.wtf>
2025-07-10 21:56:41 -0400

    Changes values

--- core/src/banking_stage/transaction_scheduler/bam_scheduler.rs
+++ core/src/banking_stage/transaction_scheduler/bam_scheduler.rs
@@ -49,8 +49,8 @@ fn passthrough_priority(
     *id
 }
 
-const MAX_TXN_PER_BATCH: usize = 32;
-const MAX_SCHEDULED_PER_WORKER: usize = MAX_TXN_PER_BATCH;
+const MAX_TXN_PER_BATCH: usize = 8;
+const MAX_SCHEDULED_PER_WORKER: usize = MAX_TXN_PER_BATCH * 4;
 
 pub struct BamScheduler<Tx: TransactionWithMeta> {
     workers_scheduled_count: Vec<usize>,
