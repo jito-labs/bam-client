77d4f4f9e411b7be5b6243d4b0c910a05561c594
seg <zano@jito.wtf>
2025-07-17 14:28:45 -0400

    BAM patch
    Fixes bam-url not properly wired up on startup (#153)
    
    Remove startup grace period, remove chunking into container, and fix tests not exiting (#150)
    
    Adds BAM local cluster testing (#149)
    
    Create tagged releases with submodules  (#143)
    
    Run blacklisted account check in bam receive and buffer (#155)
    
    Add bam client to the client version in gossip (#164)
    
    Audit: add signature verification to incoming packets (#144)
    
    Audit: Add sigverify into incoming packets
    
    Audit: filter votes (#173)
    
    [Audit] Use bank forks structure to assess priority fees (#151)
    
    Audit: Clear prio graph in between slots (#176)
    
    Audit: reset BAM state (#177)
    
    feat(validator): Add default port handling for BAM URLs (#180)
    
    (BP #183 v2.3): Cleanup for CI testing (#184)
    
    Batched Messages BP (BP #182 v2.3)
    
    Fix TPU Info For Fallback Mode (BP #187 v2.3)
    
    Audit: add domain separation (BP #179 v2.3)
    
    feat(bam-local-cluster): Configurable port range block start (BP #188 v2.3) (#190)
    
    Create full source binary during release process (BP #194 v2.3)
    
    [Audit] Low sev audit fixes (BP #196 v2.3)
    
    More Scheduler Filters + Metrics (#206)
    
    Fix v2.3 CI (#217)
    
    Don't lock POH so much (#220)
    
    Disable bam payments (#237)
    
    Simple BAM Receive + Scheduling Benchmark (#218)
    
    Fix master CI (#219)
    
    Batch Receive, Pre-verification Checks and Sigverify in BAM Receive and Buffer (#210)
    
    Receive And Buffer: Parse In Separate Thread (#216)
    
    updating the mergify config to only allow the engineering team within… (#229)
    
    Stress test BAM benchmark (#226)
    
    Simplified Scheduler + Small Tweaks (#221)
    
    New repo name (#233)
    
    Fix Container Capacity Check (Audit Issue #92) (#240)
    
    Update BAM protos submodule URL to use HTTPS (BP v2.3 #245) (#251)
    
    Adding legal tos (bp v2.3 #244)
    
    Small Log Clean-up (bp v2.3 #248)

--- scripts/dcou-tainted-packages.sh
+++ scripts/dcou-tainted-packages.sh
@@ -3,6 +3,7 @@
 # shellcheck disable=SC2034 # This file is intended to be `source`d
 declare dcou_tainted_packages=(
   solana-accounts-bench
+  bam-banking-bench
   solana-banking-bench
   agave-ledger-tool
   solana-bench-tps
