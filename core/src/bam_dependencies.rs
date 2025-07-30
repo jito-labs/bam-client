2b1d49c121bc87ad6d402d1c132cd764ca67f2f1
David Gorski <david@jito.wtf>
2025-07-29 20:59:29 -0400

    Fix and clean-up

--- core/src/bam_dependencies.rs
+++ core/src/bam_dependencies.rs
@@ -1,35 +1,42 @@
 /// Dependencies that are needed for the BAM (Jito Scheduler Service) to function.
 /// All-in-one for convenience.
 use std::sync::{atomic::AtomicBool, Arc, Mutex};
+use jito_protos::proto::bam_types;
+
 use {
     crate::proxy::block_engine_stage::BlockBuilderFeeInfo,
     jito_protos::proto::{
         bam_api::{
-            start_scheduler_message::VersionedMsg, StartSchedulerMessage, StartSchedulerMessageV0,
+            scheduler_message::VersionedMsg, SchedulerMessage, SchedulerMessageV0,
         },
-        bam_types::AtomicTxnBatch,
     },
     solana_gossip::cluster_info::ClusterInfo,
     solana_pubkey::Pubkey,
 };
 
+pub enum BamOutboundMessage {
+    AtomicTxnBatchResult(bam_types::AtomicTxnBatchResult),
+    Heartbeat(bam_types::ValidatorHeartBeat),
+    LeaderState(bam_types::LeaderState),
+}
+
 #[derive(Clone)]
 pub struct BamDependencies {
     pub bam_enabled: Arc<AtomicBool>,
 
-    pub batch_sender: crossbeam_channel::Sender<AtomicTxnBatch>,
-    pub batch_receiver: crossbeam_channel::Receiver<AtomicTxnBatch>,
+    pub batch_sender: crossbeam_channel::Sender<bam_types::AtomicTxnBatch>,
+    pub batch_receiver: crossbeam_channel::Receiver<bam_types::AtomicTxnBatch>,
 
-    pub outbound_sender: crossbeam_channel::Sender<StartSchedulerMessageV0>,
-    pub outbound_receiver: crossbeam_channel::Receiver<StartSchedulerMessageV0>,
+    pub outbound_sender: crossbeam_channel::Sender<BamOutboundMessage>,
+    pub outbound_receiver: crossbeam_channel::Receiver<BamOutboundMessage>,
 
     pub cluster_info: Arc<ClusterInfo>,
     pub block_builder_fee_info: Arc<Mutex<BlockBuilderFeeInfo>>,
     pub bam_node_pubkey: Arc<Mutex<Pubkey>>,
 }
 
-pub fn v0_to_versioned_proto(v0: StartSchedulerMessageV0) -> StartSchedulerMessage {
-    StartSchedulerMessage {
+pub fn v0_to_versioned_proto(v0: SchedulerMessageV0) -> SchedulerMessage {
+    SchedulerMessage {
         versioned_msg: Some(VersionedMsg::V0(v0)),
     }
 }
