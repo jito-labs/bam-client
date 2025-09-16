76a0643fe2139f266d87a757082ef3d05c5c0a1d
ebin-mathews <144146969+ebin-mathews@users.noreply.github.com>
2025-09-16 16:08:29 -0600

    Remove the priority graph clear at slot boundaries (#207)

--- core/src/banking_stage/transaction_scheduler/bam_scheduler.rs
+++ core/src/banking_stage/transaction_scheduler/bam_scheduler.rs
@@ -488,8 +488,6 @@ impl<Tx: TransactionWithMeta> BamScheduler<Tx> {
             self.send_no_leader_slot_bundle_result(seq_id);
             container.remove_by_id(next_batch_id.id);
         }
-
-        self.prio_graph.clear();
     }
 }
 
@@ -1047,7 +1045,6 @@ mod tests {
 
     #[test]
     #[ignore]
-    #[should_panic(expected = "node must exist")]
     fn test_prio_graph_clears_on_slot_boundary() {
         let TestScheduler {
             mut scheduler,
